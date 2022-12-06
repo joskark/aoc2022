a0761c0ec672dc3efa18db9bfb433c12b7d85892
Iosif Karkanis <joskark@hotmail.com>
2022-12-06 09:15:06 +0100

    Day 6

---
+++ day6/python/main.py
@@ -0,0 +1,20 @@
+from os import getcwd
+from typing import List
+from copy import deepcopy
+
+path = getcwd()+"/input"
+
+def find_marker(line: str, n: int) -> int:
+    count = 0
+    end = count + n
+    while end < len(line):
+        s = set(line[count:end])
+        if len(s) == n:
+            return count + n
+        count += 1
+        end += 1
+
+with open(path, encoding="utf-8") as f:
+    line = f.read()
+    print(f'Part 1: {find_marker(line, 4)}')
+    print(f'Part 2: {find_marker(line, 14)}')
